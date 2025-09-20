d42f43026c3c23a40a8e07dba9fc30fee8c8cade
Cristina Martinez <mcristina.mavila@gmail.com>
2025-09-19 19:45:09 -0600

    export main routes to its own directory

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
 import { secureHeaders } from "hono/secure-headers";
 import { rateLimiter } from "@/config/rate-limiter.config";
 import { log } from "@/helpers/logger.helper";
-import { register } from "@/monitoring/metrics";
-import { HTTPException } from "hono/http-exception";
+import { main } from "@/routes/main.routes";
 
 log("API is running");
 
@@ -16,27 +16,7 @@ app.use("*", cors());
 app.use("*", secureHeaders());
 app.use("*", rateLimiter());
 
-app.get("/health", (c) => {
-    return c.json({
-        status: "ok",
-        timestamp: new Date().toISOString(),
-        uptime: process.uptime(),
-    });
-});
-
-app.get("/metrics", async (c) => {
-    try {
-        const metrics = await register.metrics();
-        return c.text(metrics, 200, {
-            "Content-Type": register.contentType,
-        });
-    } catch (error) {
-        console.error("Error generating metrics:", error);
-        return c.text("Error generating metrics", 500);
-    }
-});
-
-app.get("/", (c) => c.text("Hello World"));
+app.route("/", main);
 
 app.onError((err, c) => {
     console.error("Global error handler:", err);
---
+++ src/routes/main.routes.ts
@@ -0,0 +1,28 @@
+import { register } from "@/monitoring/metrics";
+import { Hono } from "hono";
+
+const main = new Hono();
+
+main.get("/", (c) => c.text("Hello World"));
+
+main.get("/health", (c) => {
+    return c.json({
+        status: "ok",
+        timestamp: new Date().toISOString(),
+        uptime: process.uptime(),
+    });
+});
+
+main.get("/metrics", async (c) => {
+    try {
+        const metrics = await register.metrics();
+        return c.text(metrics, 200, {
+            "Content-Type": register.contentType,
+        });
+    } catch (error) {
+        console.error("Error generating metrics:", error);
+        return c.text("Error generating metrics", 500);
+    }
+});
+
+export { main };
